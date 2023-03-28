fd336792e5038636c17e34f2baa839dd18ee8ac8
DaviLeoMeda <davileo.meda@gmail.com>
2023-03-28 11:20:42 +0200

    Step 3

--- assets/main.js
+++ assets/main.js
@@ -14,12 +14,30 @@ const word1 = prompt("Enter a word");
 const word2 = prompt("Enter another word");
 
 
-console.log(word1, word2);
+// console.log(word1, word2);
 
-if (word1.length == word2.length) {
-    console.log(word1 + " é uguale a " + word2);
-} else if (word1.length < word2.length) {
-    console.log(word1 + " é minore di " + word2);
-} else if (word2.length < word1.length) {
-    console.log(word2 + " é minore di " + word1);
-}
+// if (word1.length == word2.length) {
+//     console.log(word1 + " é uguale a " + word2);
+// } else if (word1.length < word2.length) {
+//     console.log(word1 + " é minore di " + word2);
+// } else if (word2.length < word1.length) {
+//     console.log(word2 + " é minore di " + word1);
+// }
+
+
+// blocco 3
+
+
+let number = prompt("Enter a number");
+let number1 = prompt("Enter a number");
+let number2 = prompt("Enter a number");
+let number3 = prompt("Enter a number");
+let number4 = prompt("Enter a number");
+let number5 = prompt("Enter a number");
+let number6 = prompt("Enter a number");
+let number7 = prompt("Enter a number");
+let number8 = prompt("Enter a number");
+let number9 = prompt("Enter a number");
+let number10 = prompt("Enter a number");
+
+console.log(number, number1, number2, number3, number4, number5, number6, number7, number8, number9, number10);
