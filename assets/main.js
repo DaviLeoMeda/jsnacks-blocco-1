bd27ad173af55ba50b6b67885a3a0f9e70dd4948
DaviLeoMeda <davileo.meda@gmail.com>
2023-03-28 11:04:36 +0200

    Step 1

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+let numb1 = prompt("Enter a number");
+let numb2 = prompt("Enter another number");
+
+console.log(numb1, numb2);
+
+if (numb1 > numb2) {
+    console.log(numb1 + " é maior que " + numb2)
+
+} else (numb2 < numb1) {
+    console.log(numb2 + " é maior que " + numb1)
+}
